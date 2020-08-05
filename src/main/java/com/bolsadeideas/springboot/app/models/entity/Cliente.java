44a0cd866b8bcc6dd8c17f6afc36d2ce1664cb0a
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-05 14:57:09 -0400

    configurations and preparyng the aplication

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -41,6 +41,8 @@ public class Cliente implements Serializable {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createAt;
 
+    private String foto;
+
     public Long getId() {
         return id;
     }
@@ -85,8 +87,11 @@ public class Cliente implements Serializable {
         return serialVersionUID;
     }
 
+    public String getFoto() {
+        return foto;
+    }
 
-
-
-
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }

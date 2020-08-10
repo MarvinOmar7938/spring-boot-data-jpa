2f4b9732e5f3a960eabd5ae08c3fdfbcbfd0c393
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-10 16:49:18 -0400

    query JPA to find products in autocomplete

--- src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
@@ -1,7 +1,9 @@
 package com.bolsadeideas.springboot.app.models.service;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.dao.IProductoDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.entity.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,6 +22,9 @@ public class ClienteServiceImpl implements IClienteService{
 
     @Autowired
     private IClienteDao clienteDao;
+
+    @Autowired
+    private IProductoDao productoDao;
 
     @Override
     @Transactional(readOnly = true)
@@ -49,5 +54,11 @@ public class ClienteServiceImpl implements IClienteService{
     @Transactional
     public void delete(Long id) {
         clienteDao.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByNombre(String term) {
+        return productoDao.findByNombreLikeIgnoreCase("%"+term+"%");
     }
 }
--- src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.app.models.service;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.entity.Producto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -16,4 +17,5 @@ public interface IClienteService {
     public void save(Cliente cliente);
     public Cliente findOne(Long id);
     public void delete(Long id);
+    public List<Producto> findByNombre(String term);
 }

d9cbe2c4b824fa4c3a9031acc9268b4fa2fa068d
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-12 11:58:44 -0400

    create method ver for details in FacturaController

--- src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -21,4 +21,6 @@ public interface IClienteService {
     public List<Producto> findByNombre(String term);
     public void saveFactura(Factura factura);
     public Producto findProductoById(Long id);
+    public Factura findFacturaById(Long id);
+
 }

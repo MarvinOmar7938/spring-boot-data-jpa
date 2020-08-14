61a44b69dbca8198da24e840c8f963e68fec9bf7
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-14 12:02:43 -0400

    handling access denied errors

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -3,10 +3,8 @@ package com.bolsadeideas.springboot.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.nio.file.Paths;
 
 /**
  * @author Marvin Tola
@@ -27,5 +25,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**").addResourceLocations(resourcePath);
     }*/
 
-
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/error_403").setViewName("error_403");
+    }
 }

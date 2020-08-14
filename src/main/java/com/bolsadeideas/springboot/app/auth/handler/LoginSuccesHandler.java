a34b2d1b109e0bb6c4efbf6719c410ae835c3123
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-14 16:08:40 -0400

    getting authenticated user in controller 1

--- src/main/java/com/bolsadeideas/springboot/app/auth/handler/LoginSuccesHandler.java
+++ src/main/java/com/bolsadeideas/springboot/app/auth/handler/LoginSuccesHandler.java
@@ -22,9 +22,13 @@ public class LoginSuccesHandler extends SimpleUrlAuthenticationSuccessHandler {
 
         SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
         FlashMap flashMap = new FlashMap();
-        flashMap.put("success","Ha iniciado sesion con exito!");
+        flashMap.put("success","Hola "+authentication.getName()+", haz iniciado sesion con exito!");
 
         flashMapManager.saveOutputFlashMap(flashMap,request,response);
+
+        if(authentication != null){
+            logger.info("El usuario '"+authentication.getName()+"' ha iniciado sesion con exito");
+        }
 
         super.onAuthenticationSuccess(request, response, authentication);
     }

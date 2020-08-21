f5e3c73480ce6684743c88fa83faf6731917a520
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-21 16:32:14 -0400

    create controller LocaleController

---
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/LocaleController.java
@@ -0,0 +1,22 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Marvin Tola
+ * @email mtolapacheco@gmail.com
+ */
+
+@Controller
+public class LocaleController {
+
+    @GetMapping("/locale")
+    public String locale(HttpServletRequest request){
+        String ultimaUrl = request.getHeader("referer");
+
+        return "redirect:".concat(ultimaUrl);
+    }
+}

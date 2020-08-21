09f7a4a8bb92ee6262e23c602e1697b677db0c05
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-21 16:44:01 -0400

    translation in controllers

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -7,6 +7,7 @@ import com.bolsadeideas.springboot.app.util.paginator.PageRender;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -34,6 +35,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -52,6 +54,10 @@ public class ClienteController {
 
     @Autowired
     private IUploadFileService uploadFileService;
+
+    @Autowired
+    private MessageSource messageSource;
+
 
     @Secured({"ROLE_USER"})
     @GetMapping(value = "/uploads/{filename:.+}")
@@ -82,7 +88,7 @@ public class ClienteController {
     }
 
     @RequestMapping(value ={"listar","/"},method = RequestMethod.GET)
-    public String listar(@RequestParam(name="page",defaultValue = "0") int page, Model model, Authentication authentication, HttpServletRequest request){
+    public String listar(@RequestParam(name="page",defaultValue = "0") int page, Model model, Authentication authentication, HttpServletRequest request, Locale locale){
 
 
         if(authentication != null){
@@ -120,7 +126,7 @@ public class ClienteController {
         Page<Cliente> clientes = clienteService.findAll(pageRequest);
 
         PageRender<Cliente> pageRender = new PageRender<>("/listar",clientes);
-        model.addAttribute("titulo","Listado de clientes");
+        model.addAttribute("titulo",messageSource.getMessage("text.cliente.listar.titulo",null,locale));
         model.addAttribute("clientes",clientes);
         model.addAttribute("page",pageRender);
         return "listar";

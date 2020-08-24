b94c98da70ef907f912e989ce98d80fefa2872d4
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-24 18:03:55 -0400

    create link PdfView for export the factura pdf

--- src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
@@ -24,14 +24,16 @@ public class FacturaPdfView extends AbstractPdfView {
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
         Factura factura = (Factura) model.get("factura");
         PdfPTable tabla = new PdfPTable(1);
+        tabla.setSpacingAfter(20);
         tabla.addCell("Datos del Cliente");
         tabla.addCell(factura.getCliente().getNombre() + " "+factura.getCliente().getApellido());
         tabla.addCell(factura.getCliente().getEmail());
 
         PdfPTable tabla2 = new PdfPTable(1);
+        tabla2.setSpacingAfter(20);
         tabla2.addCell("Datos de la factura");
         tabla2.addCell("Folio: "+ factura.getId());
-        tabla2.addCell("Descripcion "+factura.getDescripcion());
+        tabla2.addCell("Descripcion: "+factura.getDescripcion());
         tabla2.addCell("Fecha: "+factura.getCreateAt());
 
         document.add(tabla);

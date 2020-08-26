0f2e8c852b83847d00a2aaccae6e96ad0d54a8cb
MarvinOmar7938 <54182614+MarvinOmar7938@users.noreply.github.com>
2020-08-26 16:22:48 -0400

    shape using @ResponseBody annotation over the handler method

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
 import com.bolsadeideas.springboot.app.models.service.IUploadFileService;
 import com.bolsadeideas.springboot.app.util.paginator.PageRender;
+import com.bolsadeideas.springboot.app.view.xml.ClienteList;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -85,6 +87,11 @@ public class ClienteController {
         model.put("titulo","Detalle cliente: "+cliente.getNombre());
 
         return "ver";
+    }
+
+    @GetMapping(value = "/listar-rest")
+    public @ResponseBody ClienteList listarRest(){
+        return new ClienteList(clienteService.findAll());
     }
 
     @RequestMapping(value ={"listar","/"},method = RequestMethod.GET)
